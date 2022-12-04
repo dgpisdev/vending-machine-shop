59231eedae19312a23e0b5e96a69dcf846a58aa8
dgpisdev <dgp0920@outlook.com>
2022-12-04 14:07:47 +0200

    optimized code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function submitForm() {
-  const yourName = document.getElementById("name-input").value;
-  const emailAdress = document.getElementById("email-input").value;
-  const phoneNumber = document.getElementById("tel-input").value;
-  const subjectMessage = document.getElementById("subject-input");
-  const yourMessage = document.getElementById("text-area").value;
+  const numeInput = document.getElementById("nume-input").value;
+  const adresaEmail = document.getElementById("email-input").value;
+  const numarTelefon = document.getElementById("tel-input").value;
+  const subiectMesaj = document.getElementById("subiect-input");
+  const textArea = document.getElementById("text-area").value;
   const completatGresit = "Trebuie sa completezi toate campurile obligatorii!";
   const completatCorect = "Mesajul dumneavoastra a fost transmis cu succes!";
   const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -11,27 +11,27 @@ function submitForm() {
   var y = document.getElementById("completat-gresit");
   x.className = "show";
   y.className = "show";
-  if ((yourName == "") | (emailAdress == "") | (yourMessage == "")) {
+  if ((numeInput == "") | (adresaEmail == "") | (textArea == "")) {
     document.getElementById("completat-gresit").innerHTML = completatGresit;
     setTimeout(function () {
       y.className = y.className.replace("show", "");
-    }, 10000);
-  } else if (emailAdress.match(mailformat)) {
+    }, 5000);
+  } else if (adresaEmail.match(mailformat)) {
     document.getElementById("completat-corect").innerHTML = completatCorect;
     setTimeout(function () {
       x.className = x.className.replace("show", "");
-    }, 3000);
-    document.getElementById("name-input").value = "";
+    }, 5000);
+    document.getElementById("nume-input").value = "";
     document.getElementById("email-input").value = "";
     document.getElementById("text-area").value = "";
     document.getElementById("tel-input").value = "";
-    document.getElementById("subject-input").value = "";
+    document.getElementById("subiect-input").value = "";
     document.getElementById("completat-gresit").innerHTML = "";
   } else {
     document.getElementById("completat-gresit").innerHTML =
       "Adresa de e-mail introdusa nu este valida!";
     setTimeout(function () {
       y.className = y.className.replace("show", "");
-    }, 10000);
+    }, 5000);
   }
 }
